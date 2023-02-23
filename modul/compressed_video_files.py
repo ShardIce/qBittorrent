171e42a6ed3a5f58a642d770a06d8f982d9d85b4
ShardIce <48800677+ShardIce@users.noreply.github.com>
2023-02-22 17:37:01 -0800

    Update compressed_video_files.py
    сжатие идёт на 15 секунд для тестирования, если удалить строчку ```"-ss", "00:00:00", "-t", "00:00:15",``` то сжатие будет полным

--- modul/compressed_video_files.py
+++ modul/compressed_video_files.py
@@ -1,155 +1,82 @@
-# -*- encoding: utf-8 -*-
 import os
+from PIL import Image
+import subprocess
 import logging
-import csv
+import time
 import platform
 from jproperties import Properties
-import subprocess
-import time
-from datetime import timedelta
-import telegram
 
 configs = Properties()
 with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'local.properties'), 'rb') as read_prop:
     configs.load(read_prop)
-    prop_view = configs.items()
+prop_view = configs.items()
 
-    if platform.system() == 'Windows':
-        TORRENT_PATH = configs.get("torrent_path_win").data
-    if platform.system() == 'Darwin':
-        TORRENT_PATH = configs.get("torrent_path_mac").data
-    if platform.system() == 'Linux':
-        TORRENT_PATH = configs.get("torrent_path_unix").data
+if platform.system() == 'Windows':
+    TORRENT_PATH = configs.get("torrent_path_win").data
+if platform.system() == 'Darwin':
+    TORRENT_PATH = configs.get("torrent_path_mac").data
+if platform.system() == 'Linux':
+    TORRENT_PATH = configs.get("torrent_path_unix").data
 
-input_path = os.path.join(TORRENT_PATH, configs.get("torrent_renamed_dirname").data)
-output_path = os.path.join(TORRENT_PATH, configs.get("torrent_renamed_dirname").data)
-MEDIA_OUTPUT = os.path.join(TORRENT_PATH, configs.get("media_output_dirname").data)
-CSV_SAVE_PATH = os.path.join(TORRENT_PATH, configs.get("torrent_renamed_dirname").data, "compress_video.csv")
+FILES_INPUT = os.path.join(TORRENT_PATH, configs.get("torrent_renamed_dirname").data)  # Ввод переименованных файлов
+FILES_OUTPUT = os.path.join(TORRENT_PATH, configs.get("compress_dirname").data) # Вывод сжатых файлов
+MEDIA_OUTPUT = os.path.join(TORRENT_PATH, configs.get("media_output_dirname").data)  # Медиафайлы для загруженных постеров
+LOG_SAVE_PATH = os.path.join(TORRENT_PATH, configs.get("logs_dirname").data) # Путь к логам
 
-# настройка логирования
-log_path = os.path.join(TORRENT_PATH, configs.get("torrent_renamed_dirname").data, "my_log_file.log")
-logging.basicConfig(filename=log_path, level=logging.ERROR)
+def get_poster_extensions(video_filename):
+    poster_extensions = []
+    for ext in ['png', 'gif', 'webp']: # изменяем на список необходимых расширений
+        poster_path = os.path.join(MEDIA_OUTPUT, os.path.splitext(video_filename)[0] + "." + ext)
+        if os.path.exists(poster_path):
+            poster_extensions.append(ext)
+    return poster_extensions
 
-# Настройки ffmpeg
-num_processors = 4
-memory = 10
-codec = 'libx264'  # Нужный нам кодек. Увидеть все введите в косоли ffmpeg -codecs рабочий: libx265, libx264, hevc h264_nvenc
-qscale = 1  # Значение 1 соответствует самому лучшему качеству, а 31 — самому худшему.
-format_video = 'mkv'  # поменять контейнер c формат m4v на mkv
-crf = 24  # Cтепень сжатия. Чем меньше число, тем лучше качество, но больше размер.
 
-# Настройки бота для телеграмм
-telegram_bot_token = 'telegram_bot_token'
-telegram_chat_id = 'telegram_chat_id'
+def compress_video(FILES_INPUT, output_folder):
+    # Настройка логирования
+    log_path = os.path.join(LOG_SAVE_PATH, "compress_video.log")
+    logger = logging.getLogger("compress_video")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-# Проверьте, является ли файл видеофайлом
-def is_video_file(filename):
-    video_file_extensions = [".mp4", ".avi", ".mkv", ".mov", ".wmv"]
-    return any(filename.endswith(extension) for extension in video_file_extensions)
+    for filename in os.listdir(FILES_INPUT):
+        if filename.lower().endswith(('.avi', '.mp4', '.mkv', '.mov', '.flv', '.wmv')): # список расширений можно расширить по необходимости
+            video_path = os.path.join(FILES_INPUT, filename)
+            filename_utf8 = filename.encode('utf-8')
+            output_path = os.path.join(output_folder, os.path.splitext(filename_utf8.decode())[0] + ".mkv")
+            poster_extensions = get_poster_extensions(filename)
 
-# Сжать видео файл
-def compress_video(filename):
-    input_file = os.path.join(input_path, filename)
-    output_folder = os.path.join(output_path, os.path.splitext(filename)[0])
-    file_name_path = f"{output_folder}/compressed-{filename}"
-    image_poster = os.path.join(MEDIA_OUTPUT, os.path.splitext(filename)[0] + '.jpg')
+            if len(poster_extensions) > 0:
+                poster_path = os.path.join(MEDIA_OUTPUT, os.path.splitext(filename)[0] + "." + poster_extensions[0])
+            else:
+                poster_path = os.path.join(MEDIA_OUTPUT, os.path.splitext(filename)[0] + ".jpg")
 
-    if not os.path.exists(input_file):
-        print(f"Error: Input file {input_file} does not exist")
-        return filename, 0, 0
+            logger.info("Video file: %s", filename)
+            logger.info("Poster path: %s", poster_path)
 
-    # Проверьте, является ли входной файл видеофайлом
-    if not is_video_file(filename):
-        return filename, 0, 0
+            # Старт времени начало сжатия
+            start_time = time.time()
 
-    # Создайте выходную папку, если она не существует
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+            # Сжать видео файл с помощью ffmpeg
+            # Сжать видео файл и добавить постер
+            command = ["ffmpeg", "-i", video_path, "-i", poster_path, "-c:v", "libx264", "-crf", "23", "-preset",
+                       "medium", "-c:a", "copy", "-vf", "scale=trunc(iw/2)*2:trunc(ih/2)*2", "-attach", poster_path,
+                       "-metadata:s:t", "mimetype=image/jpeg", "-metadata:s:t", "filename=poster.jpg", "-metadata",
+                       "comment=Compressed with Python script", "-map", "0:v", "-map", "0:a", "-ss", "00:00:05", "-t", "00:00:15", "-shortest", output_path]
 
-    # Получаем размер исходного файла
-    input_size = round(os.path.getsize(input_file) / (10 ** 9), 3)
+            try:
+                subprocess.call(command)
+            except Exception as e:
+                logger.error("Error compressing video: %s", str(e), exc_info=True)
+                continue
 
-    # Старт времени начало сжатия
-    start_time = time.time()
+            # Добавление информации в log
+            log_info = f"{filename} - size before: {os.path.getsize(video_path)} bytes, " \
+                       f"size after: {os.path.getsize(output_path)} bytes"
+            logger.info(log_info)
 
-    # Сжать видеофайл с помощью указанного кодека -an -map 0:a -g 30
-    cmd = f"ffmpeg -loop 1 -i \"{image_poster}\" \"{input_file}\" -c:v {codec} -c:a copy -threads {num_processors} -crf {crf} -q:v {qscale} -f matroska -y \"{file_name_path}\""
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    # Конец измерения времени сжатия
-    end_time = time.time()
-
-    # Расчет времени сжатия
-    compression_time = end_time - start_time
-
-    # Запишем данные в формате ЧЧ:ММ:СС
-    time_in_hhmmss = str(timedelta(seconds=compression_time)).split(".")[0]
-    print(time_in_hhmmss)
-
-    # Проверьте наличие ошибок во время сжатия
-    if result.returncode != 0:
-        print(f"Error: Failed to compress {filename}")
-        print(result.stderr.decode())
-        return filename, 0, 0, time_in_hhmmss
-
-    # Получаем размер выходных файлов
-    output_size = round(os.path.getsize(file_name_path) / (10 ** 9), 3)
-
-     # Удалить исходный файл
-    # os.remove(input_file)
-
-    return filename, input_size, output_size, time_in_hhmmss
-
-async def send_telegram_notification(filename, time_in_hhmmss):
-    try:
-        bot = telegram.Bot(token=telegram_bot_token)
-        time_delta = timedelta(hours=0, minutes=0, seconds=0)
-        time_list = time_in_hhmmss.split(":")
-        if len(time_list) == 3:
-            time_delta = timedelta(hours=int(time_list[0]), minutes=int(time_list[1]), seconds=int(time_list[2]))
-        elif len(time_list) == 2:
-            time_delta = timedelta(hours=0, minutes=int(time_list[0]), seconds=int(time_list[1]))
-        elif len(time_list) == 1:
-            time_delta = timedelta(hours=0, minutes=0, seconds=int(time_list[0]))
-        else:
-            raise ValueError(f"Invalid time format: {time_in_hhmmss}")
-        total_seconds = time_delta.total_seconds()
-        message = f"Видео было успешно сжато! \n{filename} \nВремя сжатия: \n{str(timedelta(seconds=total_seconds))}"
-        await bot.send_message(chat_id=telegram_chat_id, text=message)
-    except Exception as e:
-        logging.error(f"Error occurred while sending telegram notification for file {filename}: {str(e)}")
-
-# Сохраните информацию о сжатом видео в файл CSV.
-def save_to_csv(data):
-    try:
-        with open(CSV_SAVE_PATH, "a", encoding="UTF-8", newline="") as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-
-            # Проверьте, существует ли файл CSV
-            if os.path.exists(CSV_SAVE_PATH) and os.path.getsize(CSV_SAVE_PATH) == 0:
-                writer.writerow(["Filename", "Input Size (GB)", "Output Size (GB)", "Compression Time"])
-
-            writer.writerows(data)
-            # csvfile.close()
-
-    except Exception as e:
-        print(f"Error: Failed to write to CSV file {CSV_SAVE_PATH}. Error: {e}")
-
-if __name__ == "__main__":
-    import asyncio
-
-    # Получить список всех видеофайлов во входном пути
-    video_files = [file for file in os.listdir(input_path) if is_video_file(file)]
-    compressed_video_data = []
-
-    # Сжать каждый видеофайл
-    for file in video_files:
-        filename, input_size, output_size, time_in_hhmmss = compress_video(file)
-        compressed_video_data.append([filename, input_size, output_size, time_in_hhmmss])
-
-    # Сохраните сжатые видеоданные в файл CSV.
-    save_to_csv(compressed_video_data)
-
-    asyncio.run(send_telegram_notification(filename, time_in_hhmmss))
-
-    print("Video compression and saving to CSV file completed.")
+compress_video(FILES_INPUT, FILES_OUTPUT)
